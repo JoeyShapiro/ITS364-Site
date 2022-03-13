f1c033762b67d26f354e49c5adc68ef459125f5d
JoeyShapiro <joeyashapiro@gmail.com>
2022-03-13 13:01:08 -0500

    can access db

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql2');
 const fs = require('fs');
-const { database } = require('pg/lib/defaults');
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'))
 
 var settings = JSON.parse(fs.readFileSync('secret.json', 'utf8')); // could just make this config
 
-const serverCa = [fs.readFileSync("BaltimoreCyberTrustRoot.crt.pem", 'utf-8')];
+const serverCa = [fs.readFileSync("DigiCertGlobalRootCA.crt.pem", 'utf-8')];
 var config =
 {
    host     : settings['host'],
@@ -20,31 +23,17 @@ var config =
 
 const conn = new mysql.createConnection(config);
 
-conn.connect(
-    function (err) { 
-        if (err) { 
-            console.log("!!! Cannot connect !!! Error:");
-            throw err;
-        }
-        else {
-            console.log("Connection established.");
-            readData();
-        }
-    });
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
 
-function readData(){
-    conn.query('SELECT * FROM artist', 
-        function (err, results, fields) {
-            if (err) throw err;
-            else console.log('Selected ' + results.length + ' row(s).');
-            for (i = 0; i < results.length; i++) {
-                console.log('Row: ' + JSON.stringify(results[i]));
-            }
-            console.log('Done.');
-        })
-    conn.end(
-        function (err) { 
-            if (err) throw err;
-            else  console.log('Closing connection.') 
+
+app.get('/', function (req, res) {
+    conn.connect();
+    conn.query('SELECT * FROM artist', function(err, rows, fields)   
+    {  
+        if (err) throw err;
+        res.json(rows); 
     });
-};
+    conn.end();
+});
